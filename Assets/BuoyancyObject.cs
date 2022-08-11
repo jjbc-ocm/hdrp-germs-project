420663f19c77fdb22166b133d657d493bff182e5
@jilmer1 <jilmer@legendsofthepit.com>
2022-08-11 20:35:05 +0800

    Update positionings

--- Assets/BuoyancyObject.cs
+++ Assets/BuoyancyObject.cs
@@ -10,7 +10,7 @@ public class BuoyancyObject : MonoBehaviour
     public float airDrag = 0.0f;
     public float airAngularDrag = 0.05f;
     public float floatingPower = 15f;
-    public float waterHeight = 0f;
+    public float offsetY;
 
     Rigidbody m_Rigidbody;
 
@@ -18,18 +18,31 @@ public class BuoyancyObject : MonoBehaviour
 
     bool underwater;
 
+    private float deltaTime;
+
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(Mathf.Sin(deltaTime) * 5f, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
     void FixedUpdate()
     {
+        var water = WaterManager.Instance;
+
+        var waterHeight = water ? water.transform.position.y : 0;
+
         floatersUnderwater = 0;
 
         for (int i = 0; i < floaters.Length; i++)
         {
-            float difference = floaters[i].position.y - waterHeight;
+            float difference = floaters[i].position.y - waterHeight + offsetY;
 
             if (difference < 0)
             {
