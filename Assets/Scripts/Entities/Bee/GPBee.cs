69aaa5aa672032858f68e3f69a4afbdcc0bc17ac
MickOrdaz <mickordaz@gmail.com>
2022-11-07 19:40:52 -0600

    Bee monster added and placed in map (bee movement is not ready yet). Mosnter animation sync improved. Monsters life poitns tweaked. SFX for monster dying, being hurt and diong melee hit implemented.

---
+++ Assets/Scripts/Entities/Bee/GPBee.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TanksMP;
+using Photon.Pun;
+
+public class GPBee : GPMonsterBase
+{
+    BEE_STATES m_currentState = BEE_STATES.kIdle;
+    public enum BEE_STATES
+    {
+        kIdle,
+        kAttacking,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            m_health.OnDieEvent.AddListener(OnDie);
+            m_health.OnDamagedEvent.AddListener(OnDamage);
+            m_detectionTrigger.m_OnEnterEvent.AddListener(OnPlayerEnter);
+            m_detectionTrigger.m_OnExitEvent.AddListener(OnPlayerExit);
+            m_meleeDamageTrigger.m_OnEnterEvent.AddListener(BitePlayer);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Only master client will execute enemy logic and share the data to other players
+        if (!PhotonNetwork.IsMasterClient || m_health.m_isDead)
+        {
+            return;
+        }
+
+        switch (m_currentState)
+        {
+            case BEE_STATES.kIdle:
+                OnIdle();
+                break;
+            case BEE_STATES.kAttacking:
+                OnAttacking();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnIdle()
+    {
+        if (m_currTargetPlayer != null)
+        {
+            m_nextAttackTimeCounter += Time.deltaTime;
+            if (m_nextAttackTimeCounter > m_nextAttackTime)
+            {
+                m_nextAttackTimeCounter = 0.0f;
+                m_nextAttackTime = Random.Range(m_minAttackTime, m_maxAttackTime);
+                ChangeState(BEE_STATES.kAttacking);
+            }
+
+            if (!m_tweening)
+            {
+                LookAtTarget(m_currTargetPlayer.transform);
+            }
+
+        }
+    }
+
+    void OnAttacking()
+    {
+        string triggerToActivate = "";
+
+        Vector3 targetDir = m_currTargetPlayer.transform.position - transform.position;
+        targetDir.y = 0.0f;
+        if (targetDir.magnitude <= m_meleeRadius) // pick melee attack
+        {
+            triggerToActivate = PickAttack(m_meleeAtkTriggerNames);
+        }
+        else if (m_projectileAtkTriggerNames.Count > 0)//pick range attack
+        {
+            triggerToActivate = PickAttack(m_projectileAtkTriggerNames);
+        }
+
+        m_photonView.RPC("RPCPlayAnimationTrigger", RpcTarget.All, triggerToActivate);
+
+        ChangeState(BEE_STATES.kIdle);
+    }
+
+    void ChangeState(BEE_STATES newState)
+    {
+        if (m_currentState == BEE_STATES.kIdle && newState == BEE_STATES.kAttacking)
+        {
+            //look at target
+            /*
+            Vector3 lookDir = m_currTargetPlayer.transform.position - transform.position;
+            lookDir.y = 0.0f;
+            m_tweening = true;
+            LeanTween.rotate(gameObject, Quaternion.LookRotation(lookDir).eulerAngles, 0.5f).setEaseSpring().setOnComplete(OnTweenEnd);
+            */
+        }
+        else if (m_currentState == BEE_STATES.kAttacking && newState == BEE_STATES.kIdle)
+        {
+            //choose new target
+            ChoosePlayerToAttack();
+        }
+
+        m_currentState = newState;
+    }
+
+}
