cfa605b29072eb73d395c4ec09e4c0feebd090ed
@jilmer1 <jilmer@legendsofthepit.com>
2022-12-05 20:34:33 +0800

    Update 12/5/2022

--- Assets/_Main/Scripts/UI/LoadingUI.cs
+++ Assets/_Main/Scripts/UI/LoadingUI.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingUI : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text textMessage;
 
+    [SerializeField]
+    private Slider sliderProgress;
+
     public string Text { get; set; }
+
+    public float Progress { get; set; }
 
     void Update()
     {
         textMessage.text = Text;
+
+        sliderProgress.value = Progress;
     }
 }
