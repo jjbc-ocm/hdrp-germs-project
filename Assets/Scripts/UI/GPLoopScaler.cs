4fbe6b68e4ef8d1404a767b3d6165243d782f894
MickOrdaz <mickordaz@gmail.com>
2022-12-14 20:28:21 -0600

    -Bug fixed where waiting room background disapeared when playing on other resolutions. -Bug fixed where the home button of the dummy selection screen was not working. -Join game button of the waiting room not has a looped scale animation to be more llamative. -Sound effects implemented on the waiting room. -Photon room is closed after battle starts so other players can't join in the middle of a ongoing battle. -Bug fixed where only master client could activate the dev cheat.

---
+++ Assets/Scripts/UI/GPLoopScaler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPLoopScaler : MonoBehaviour
+{
+  public LeanTweenType m_tweenType;
+  public float m_tweenDuration = 0.2f;
+  public float m_targetScaleX = 1.4f;
+  public float m_targetScaleY = 1.4f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    LeanTween.scaleY(gameObject, m_targetScaleY, m_tweenDuration).setEase(m_tweenType).setLoopPingPong();
+    LeanTween.scaleX(gameObject, m_targetScaleX, m_tweenDuration).setEase(m_tweenType).setLoopPingPong();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
