55a5e33cf76755e3f69e6491f5e36f97343cfd9c
MickOrdaz <mickordaz@gmail.com>
2022-12-24 06:16:07 -0600

    -Setted up a vertical layout group to the game object that holds the friend slots. -Setted up a mask so the friend slots dont overflow the window when they are many. -Created a simple class for holding friend data in the meantime so I can use it to test filling the friend list UI. -Methods for spawning friend slots prefab on the friend list UI added. -Method for displaying the friend data on the friend slots added.

---
+++ Assets/Scripts/Social/GPFriend.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Very simple class to just hold some basic data in the meantime.
+/// </summary>
+public class GPFriend
+{
+    public string m_friendName;
+    public GP_ONLINE_STATUS m_onlineStatus;
+    public Sprite m_profileIcon;
+}
